8512a228fb1ccb12e72f4f5f4b4c723a9fc5c922
Dimitrije <dbajagic3@gmail.com>
2020-12-15 12:41:22 +0100

    Dodata klasa player, ispravljeno kretanje igraca

--- game.py
+++ game.py
@@ -1,30 +1,11 @@
 import sys
-from PyQt5 import QtCore,QtWidgets
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtGui import QImage, QPalette, QBrush
+from PyQt5 import QtCore, QtWidgets
+from Player import Player
+
 WINDOW_WIDTH = 600
 WINDOW_HEIGTH = 800
-
-
-class Player(QtWidgets.QLabel):
-    def __init__(self, parent = None):
-        super().__init__(parent)
-        self.setPixmap(QPixmap('assets/ship.png').scaled(50, 50))
-        
-        
-
-        
-    def keyPressEvent(self, event):
-        if (event.key() == QtCore.Qt.Key_A):
-            self.move(self.x() - 30, self.y()())
-        elif (event.key() == QtCore.Qt.Key_D):
-            self.move(self.x() + 30, self.y())
-        else:
-            QtWidgets.QLabel.keyPressEvent(self, event)
-
-
-
-
 
 
 class Window(QtWidgets.QMainWindow):
@@ -34,36 +15,26 @@ class Window(QtWidgets.QMainWindow):
         self.setWindowTitle('Space Invaders')
         self.window_width = WINDOW_WIDTH
         self.window_heigth = WINDOW_HEIGTH
-        self.setGeometry(100,100,self.window_width, self.window_heigth)
-        self.centralwidget = QtWidgets.QWidget()
-        self.setCentralWidget(self.centralwidget)
-        layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.setGeometry(100, 100, self.window_width, self.window_heigth)
+        centralwidget = QtWidgets.QWidget()
+        self.setCentralWidget(centralwidget)
+        layout = QtWidgets.QHBoxLayout(centralwidget)
         self.player = Player()
-        layout.addWidget(self.player)
 
-        
-        
+        layout.addWidget(self.player)
+        self.player.setFocus()
+
         background = QImage('assets/background.png')
         background = background.scaled(WINDOW_WIDTH, WINDOW_HEIGTH)
         pallete = QPalette()
         pallete.setBrush(QPalette.Window, QBrush(background))
         self.setPalette(pallete)
 
-        #self.player.setFocus()
-
-
-
-
-
-
 
 if __name__ == '__main__':
-
-
     app = QtWidgets.QApplication(sys.argv)
 
     window = Window()
     window.show()
-
 
     sys.exit(app.exec_())
