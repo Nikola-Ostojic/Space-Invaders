d78c85c59532ed585f7c5a63f3a37e7a6de7dabf
Milos Milovanovic <mmilos015@gmail.com>
2021-01-27 23:46:48 +0100

    FINAL VERSION

--- key_notifier.py
+++ key_notifier.py
@@ -72,12 +72,12 @@ class KeyNotifier(QObject):
                 if k == Qt.Key_Space:
                     if self.canPlayerOneShoot:
                         self.key_signal.emit(k)
-                        #self.canPlayerOneShoot = False
+                        self.canPlayerOneShoot = False
 
                 elif k == Qt.Key_Control:
                     if self.canPlayerTwoShoot:
                         self.key_signal.emit(k)
-                        #self.canPlayerTwoShoot = False
+                        self.canPlayerTwoShoot = False
                 else:
                     self.key_signal.emit(k)
             time.sleep(0.05)
